a974867fee01cbae864143a9d233d1d0933af766
Javier Maidana <Javiermaidana1234@gmail.com>
2025-02-07 15:05:30 -0300

    Añadiendo sección audio pt2

--- 14thLogic.js
+++ 14thLogic.js
@@ -1,7 +1,16 @@
-let titulo = ["Felíz San Valentín amor!❤️", "Felíz aniversario bba!❤️", "Feliz 5 años juntos bba!❤️"];
+// Variables GLOBALES
+const message = document.getElementById('typingText');
+// HEADER
+const audio = document.getElementById('music');
+const botonPlay = document.getElementById('btn-play');
+const botonStop = document.getElementById('btn-pause');
+const lyricsLetra = document.getElementById('lyrics');
+// REPRODUCTOR DE MÚSICA
+const btnBonus = document.getElementById('boton-bonus');
+const cardBonus = document.getElementById('card-bonus');
+// CARD QUE APARECE Y DESAPARECE
 
-let message = document.getElementById('typingText');
-
+// FUNCIONES
 let indiceTitulo = 0;
 
 
@@ -34,12 +43,50 @@ let erasing = () => {
      }, 50);
 }
 
-writing();
+playMusic = () => {
+    audio.play();
+    startLyrics();
+}
 
-let btnBonus = document.getElementById('boton-bonus');
-let cardBonus = document.getElementById('card-bonus');
+pauseMusic = () => {
+    audio.pause();
+    clearInterval(interval)
+}
+
+startLyrics = () => {
+
+}
+
+// Array con los títulos del header
+const titulo = ["Felíz San Valentín amor!❤️", "Felíz aniversario bba!❤️", "Feliz 5 años juntos bba!❤️"];
+
+// Objeto con el tiempo y la letra de la canción
+let letras = [
+    { time: 2, text: "Esta es la primera línea de la canción..."},
+    { time: 5, text: "segunda fraseeeeeeee..."},
+    { time: 10, text: "tercera frase de la musica"}
+]
+
+let indexLetra = 0;
+
+writing();
 
 btnBonus.addEventListener('click', () => {
     cardBonus.classList.toggle("visible");
     btnBonus.classList.toggle("rotado");
+});
+
+botonPlay.addEventListener('click', () => {
+    playMusic();
+})
+
+botonStop.addEventListener('click', () => {
+    pauseMusic();
+})
+
+audio.addEventListener('timeupdate', () => {
+    if (indexLetra < letras.length && audio.currentTime >= letras[indexLetra].time) {
+        lyricsLetra.innerHTML += `<p>${letras[indexLetra].text}</p>`;
+        indexLetra++
+    }
 });
